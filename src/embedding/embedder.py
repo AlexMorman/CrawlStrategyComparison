9d8fce67df6c302be99c5a4405ada54f8415bed3
Alex Morman <alexmorman47@gmail.com>
2026-05-02 21:00:22 -0500

    Fixed small errors

--- src/embedding/embedder.py
+++ src/embedding/embedder.py
@@ -1,15 +1,20 @@
-import config;
+from sentence_transformers import SentenceTransformer
+import config
+import torch
 
 # This class is a wrapper for the embedding model. We want the class to be model-agnostic, so that we can plug-and-play different models to tweak results
 class Embedder:
 
     def __init__(self):
-        self.model = config.EMBEDDING_MODEL;
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.batch_size = batch_size=config.EMBEDDING_BATCH_SIZE
+        self.model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2', device=self.device)
 
-    # return the embedding of a single text
+    # encode serially
     def encode(self, text):
-        return "TODO";
+        return self.model.encode(text, convert_to_numpy=True).tolist()
 
-    # return an array of the embeddings of multiple texts
+    # encode in parallel
     def encode_batch(self, texts):
-        return "TODO";
+        embeddings = self.model.encode(texts, convert_to_numpy=True, batch_size=self.batch_size, show_progress_bar=True)
+        return [e.tolist() for e in embeddings]
